cdad954cda3ca936ec048b01ed53415d8a66fe67
Calindyl <caridniss@gmail.com>
2015-08-02 12:25:05 +0200

    masodik github personal feltoltes

---
+++ js/myscript.js
@@ -0,0 +1,16 @@
+
+// <!-- Script to Activate the Carousel in 5 seconds -->
+// $('.carousel').carousel({
+//         interval: 5000 //changes the speed
+//     })
+$('#myCarousel').carousel();
+
+
+     
+//  <script type="text/javascript">
+//      $( document ).ready(function() {
+//        $('.nav li').removeClass('active');          
+//     });
+// </script> 
+
+
